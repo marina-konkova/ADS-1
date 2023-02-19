f3216e05167fb601083817ffab9e9641e9e186dc
marina-konkova <116936403+marina-konkova@users.noreply.github.com>
2023-02-19 15:33:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,46 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 1)
+    return false;
+  for (int k = 2; k <= value/2; k++) {
+    if (value % k == 0)
+      return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t number = 0;
+  uint64_t nearestnumber = 0;
+  while (n > 0) {
+    if (checkPrime(number) == true) {
+      nearestnumber = number;
+      --n;
+    }
+    ++number;
+  }
+  return nearestnumber;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  while (true) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t summa = 0;
+  uint64_t newhbound;
+  newhbound = hbound - 1;
+  while (newhbound > 0) {
+    if (checkPrime(newhbound)==true) {
+      summa += newhbound;
+    }
+    --newhbound;
+  }
+  return summa;
 }
